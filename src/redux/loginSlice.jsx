b7b1c3e4f2b3e17ebbe6ca2b66234e80e052725a
Tanjil Hossain <tanjilhossain845.com>
2023-09-07 15:59:31 +0600

    remove user id field from login slice

--- src/redux/loginSlice.jsx
+++ src/redux/loginSlice.jsx
@@ -6,7 +6,6 @@ import jwtDecode from "jwt-decode";
 const initialState = {
   is_loading: false,
   token: null,
-  userId: null,
   errorMessage: null,
   loggedInSuccess: false,
 };
@@ -19,18 +18,15 @@ const loginSlice = createSlice({
       const expiredTime = localStorage.getItem("expTime");
       if (new Date() >= new Date(parseInt(expiredTime))) {
         localStorage.removeItem("token");
-        localStorage.removeItem("userId");
         localStorage.removeItem("expTime");
       } else {
         state.token = localStorage.getItem("token");
-        state.userId = localStorage.getItem("userId");
       }
     },
     logout: (state, action) => {
       state.token = null;
       state.userId = null;
       localStorage.removeItem("token");
-      localStorage.removeItem("userId");
       localStorage.removeItem("expTime");
     },
   },
@@ -44,7 +40,6 @@ const loginSlice = createSlice({
       state.is_loading = false;
       state.errorMessage = null;
       state.token = action.payload.token;
-      state.userId = action.payload.user_id;
       state.loggedInSuccess = true;
     });
     builder.addCase(userLogin.rejected, (state, action) => {
@@ -61,12 +56,11 @@ export const userLogin = createAsyncThunk("login/post", async (values) => {
 
     const token = response.data.access;
     // decode token
-    const { exp, user_id } = jwtDecode(token);
+    const { exp } = jwtDecode(token);
     // set all needed item to localStorage
     localStorage.setItem("token", token);
     localStorage.setItem("expTime", exp * 1000);
-    localStorage.setItem("userId", user_id);
-    return { token: token, exp: exp, user_id: user_id };
+    return { token: token, exp: exp };
   } catch (error) {
     throw new Error(error.response.data.detail);
   }
